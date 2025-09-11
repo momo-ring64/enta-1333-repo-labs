aa231d6211d9cbfef72a3659d6d78e0f884f9098
momo-ring64 <tyrone_blanco@yahoo.ca>
2025-09-10 19:47:09 -0700

    add lab 2

---
+++ GD13_1333_Lab1_Blanco_Tyrone/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GD13_1333_Lab1_Blanco_Tyrone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decimal: 0 -> Binary: 0000 -> Hex: 0");
+            Console.WriteLine("Decimal: 1 -> Binary: 0001 -> Hex: 1");
+            Console.WriteLine("Decimal: 2 -> Binary: 0010 -> Hex: 2");
+            Console.WriteLine("Decimal: 4 -> Binary: 0100 -> Hex: 4");
+            Console.WriteLine("Decimal: 8 -> Binary: 1000 -> Hex: 8");
+            Console.WriteLine("Decimal: 16 -> Binary: 10000 -> Hex: 10");
+            Console.WriteLine("Decimal: 32 -> Binary: 100000 -> Hex: 20");
+            Console.WriteLine("Decimal: 64 -> Binary: 1000000 -> Hex: 40");
+            Console.WriteLine("Decimal: 100 -> Binary: 1100100 -> Hex: 64");
+            Console.WriteLine("Decimal: 255 -> Binary: 11111111 -> Hex: FF/n");
+            
+            Console.WriteLine("Greetings! My name is Tyrone and today is September 10th, 2025!");
+            Console.WriteLine("My name is Tyrone and today is September 3rd, 2025! Goodbye!");
+        }
+    }
+}
