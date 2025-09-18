19f6c5daff4d190c814df28cc73cba1d782a59cd
momo-ring64 <tyrone_blanco@yahoo.ca>
2025-09-17 19:24:57 -0700

    Lab 3 finished

---
+++ GD13_1333_Lab3_Tyrone/GD13_1333_Lab3_Tyrone/Program.cs
@@ -0,0 +1,15 @@
+﻿using GD13_1333_Lab3_Tyrone.Scripts;
+using System;
+
+namespace GD13_1333_Lab3_Tyrone
+{
+    //program class literally only starts the game
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager manager = new GameManager();
+            manager.PlayGame();
+        }
+    }
+}
